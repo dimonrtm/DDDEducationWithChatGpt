67eaa454e83f88233a4ee3db0b8681d75761dc62
dimonrtm <dimonrtm@gmail.com>
2025-08-26 20:14:10 +0500

    Solve

---
+++ UbiquitousLanguage/DDD/DomainServices/DomainServices/Interface1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainServices.Polisy
+{
+    public enum Reasons
+    {
+        Ok,
+        DifferentCrs,
+        AlreadyPublished,
+        LayerLocked,
+        ValidationFailed
+    }
+
+    public record PublicationDecision(bool IsAlloved, Reasons Reason, string? Message  = null, Dictionary<string, object>? Details = null);
+    public interface IPublicationPolicy
+    {
+        PublicationDecision CanPublish(Layer layer, LayerVersion version, PublicationContext ctx);
+    }
+}
