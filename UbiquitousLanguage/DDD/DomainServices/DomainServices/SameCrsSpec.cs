3aa14e93af919393ecaa98af3335759832c44018
dimonrtm <dimonrtm@gmail.com>
2025-08-26 09:09:03 +0500

    ~Task

---
+++ UbiquitousLanguage/DDD/DomainServices/DomainServices/SameCrsSpec.cs
@@ -0,0 +1,17 @@
+﻿public interface ISpecification<T>
+{
+    bool IsSatisfiedBy(T candidate);
+}
+
+public sealed record Layer(Guid Id, Crs Crs);
+public sealed record LayerVersion(Guid Id, Guid LayerId, Crs Crs);
+public sealed record PublicationAttempt(Layer Layer, LayerVersion Version);
+
+public sealed class SameCrsSpec : ISpecification<PublicationAttempt>
+{
+    public bool IsSatisfiedBy(PublicationAttempt attempt)
+    {
+        // TODO: верните true, если CRS совпадают; иначе false
+        throw new NotImplementedException();
+    }
+}
