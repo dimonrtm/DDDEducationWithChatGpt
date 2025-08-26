6eef45c33bf14999dcd5650ccbede7be33367ea1
dimonrtm <dimonrtm@gmail.com>
2025-08-26 20:20:27 +0500

    ~ fix from chatGpt

--- UbiquitousLanguage/DDD/DomainServices/DomainServices/Interface1.cs
+++ UbiquitousLanguage/DDD/DomainServices/DomainServices/Interface1.cs
@@ -4,20 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DomainServices.Polisy
-{
-    public enum Reasons
-    {
-        Ok,
-        DifferentCrs,
-        AlreadyPublished,
-        LayerLocked,
-        ValidationFailed
-    }
+namespace DomainServices.Policy;
 
-    public record PublicationDecision(bool IsAlloved, Reasons Reason, string? Message  = null, Dictionary<string, object>? Details = null);
-    public interface IPublicationPolicy
-    {
-        PublicationDecision CanPublish(Layer layer, LayerVersion version, PublicationContext ctx);
-    }
+public enum PublicationReason
+{
+    Ok,
+    DifferentCrs,
+    AlreadyPublished,
+    LayerLocked,
+    ValidationFailed
+}
+
+public sealed record PublicationDecision(
+    bool IsAllowed,
+    PublicationReason Reason,
+    string? Message = null,
+    IReadOnlyDictionary<string, object>? Details = null)
+{
+    public static PublicationDecision Allow(string? msg = null) =>
+        new(true, PublicationReason.Ok, msg);
+
+    public static PublicationDecision Deny(PublicationReason reason, string? msg = null,
+        IReadOnlyDictionary<string, object>? details = null) =>
+        new(false, reason, msg, details);
+}
+
+public interface IPublicationPolicy
+{
+    PublicationDecision CanPublish(Layer layer, LayerVersion version, PublicationContext ctx);
 }
